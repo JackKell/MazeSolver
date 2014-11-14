21a38e163577dab2727719525ee8c2e5a52c9226
JackKell <Jackkell@Citadel>
2014-11-13 17:08:51 -0700

    Initial Cleaning

--- MazeSolver/MazeSolver/Game/Program.cs
+++ MazeSolver/MazeSolver/Game/Program.cs
@@ -10,7 +10,7 @@ namespace MazeSolver
         /// </summary>
         static void Main(string[] args)
         {
-            using (Game1 game = new Game1())
+            using (MazeSolverGame game = new MazeSolverGame())
             {
                 game.Run();
             }
