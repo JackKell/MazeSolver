dd8b7778b90fa95977799d8a7730b967ea21f9b5
JackKell <Jackkell@Citadel>
2014-11-16 16:59:16 -0700

    GetDirection Stuff

--- MazeSolver/MazeSolver/Point.cs
+++ MazeSolver/MazeSolver/Point.cs
@@ -17,6 +17,14 @@ namespace MazeSolver
 {
     class Point
     {
+        public enum Direction
+        {
+            NORTH = 0, EAST, SOUTH, WEST,
+            NORTH_EAST, NORTH_WEST, SOUTH_EAST, SOUTH_WEST,
+            TOP, TOP_NORTH, TOP_EAST, TOP_SOUTH, TOP_WEST, TOP_NORTH_EAST, TOP_NORTH_WEST, TOP_SOUTH_EAST, TOP_SOUTH_WEST,
+            BOTTOM, BOTTOM_NORTH, BOTTOM_EAST, BOTTOM_SOUTH, BOTTOM_WEST, BOTTOM_NORTH_EAST, BOTTOM_NORTH_WEST, BOTTOM_SOUTH_EAST, BOTTOM_SOUTH_WEST,
+            NONE, ERROR
+        };
         #region Private Data Members
         private int x;
         private int y;
@@ -45,6 +53,153 @@ namespace MazeSolver
         public override string ToString()
         {
             return "(" + x + ", " + y + ", " + z + ")";
+        }
+
+        public Direction GetDirection(Point root)
+        {
+            //NORTH ASPECT
+            if (this.Y > root.Y)
+            {
+                //WEST ASPECT
+                if (this.X > root.X)
+                {
+                    //TOP ASPECT
+                    if (this.Z > root.Z)
+                        return Direction.TOP_NORTH_WEST;
+                    //BOTTOM ASPECT
+                    else if (this.Z < root.Z)
+                        return Direction.BOTTOM_NORTH_WEST;
+                    //SAME Z ASPECT
+                    else if (this.Z == root.Z)
+                        return Direction.NORTH_WEST;
+                    else { return Direction.ERROR; }
+                }
+                //EAST ASPECT
+                else if (this.X < root.X)
+                {
+                    //TOP ASPECT
+                    if (this.Z > root.Z)
+                        return Direction.TOP_NORTH_EAST;
+                    //BOTTOM ASPECT
+                    else if (this.Z < root.Z)
+                        return Direction.BOTTOM_NORTH_EAST;
+                    //SAME Z ASPECT
+                    else if (this.Z == root.Z)
+                        return Direction.NORTH_EAST;
+                    else { return Direction.ERROR; }
+                }
+                //SAME X ASPECT
+                else if (this.X == root.X)
+                {
+                    //TOP ASPECT
+                    if (this.Z > root.Z)
+                        return Direction.TOP_NORTH;
+                    //BOTTOM ASPECT
+                    else if (this.Z < root.Z)
+                        return Direction.BOTTOM_NORTH;
+                    //SAME Z ASPECT
+                    else if (this.Z == root.Z)
+                        return Direction.NORTH;
+                    else { return Direction.ERROR; }
+                }
+                else { return Direction.ERROR; }
+            }
+            //SOUTH ASPECT
+            else if (this.Y < root.Y)
+            {
+                //WEST ASPECT
+                if (this.X > root.X)
+                {
+                    //TOP ASPECT
+                    if (this.Z > root.Z)
+                        return Direction.TOP_SOUTH_WEST;
+                    //BOTTOM ASPECT
+                    else if (this.Z < root.Z)
+                        return Direction.BOTTOM_SOUTH_WEST;
+                    //SAME Z ASPECT
+                    else if (this.Z == root.Z)
+                        return Direction.SOUTH_WEST;
+                    else { return Direction.ERROR; }
+                }
+                //EAST ASPECT
+                else if (this.X < root.X)
+                {
+                    //TOP ASPECT
+                    if (this.Z > root.Z)
+                        return Direction.TOP_SOUTH_EAST;
+                    //BOTTOM ASPECT
+                    else if (this.Z < root.Z)
+                        return Direction.BOTTOM_SOUTH_EAST;
+                    //SAME Z ASPECT
+                    else if (this.Z == root.Z)
+                        return Direction.SOUTH_EAST;
+                    else { return Direction.ERROR; }
+                }
+                //SAME X ASPECT
+                else if (this.X == root.X)
+                {
+                    //TOP ASPECT
+                    if (this.Z > root.Z)
+                        return Direction.TOP_SOUTH;
+                    //BOTTOM ASPECT
+                    else if (this.Z < root.Z)
+                        return Direction.BOTTOM_SOUTH;
+                    //SAME Z ASPECT
+                    else if (this.Z == root.Z)
+                        return Direction.SOUTH;
+                    else { return Direction.ERROR; }
+                }
+                else { return Direction.ERROR; }
+            }
+            //SAME Y ASPECT
+            else if (this.Y == root.Y)
+            {
+                //WEST ASPECT
+                if (this.X > root.X)
+                {
+                    //TOP ASPECT
+                    if (this.Z > root.Z)
+                        return Direction.TOP_WEST;
+                    //BOTTOM ASPECT
+                    else if (this.Z < root.Z)
+                        return Direction.BOTTOM_WEST;
+                    //SAME Z ASPECT
+                    else if (this.Z == root.Z)
+                        return Direction.WEST;
+                    else { return Direction.ERROR; }
+                }
+                //EAST ASPECT
+                else if (this.X < root.X)
+                {
+                    //TOP ASPECT
+                    if (this.Z > root.Z)
+                        return Direction.TOP_EAST;
+                    //BOTTOM ASPECT
+                    else if (this.Z < root.Z)
+                        return Direction.BOTTOM_EAST;
+                    //SAME Z ASPECT
+                    else if (this.Z == root.Z)
+                        return Direction.EAST;
+                    else { return Direction.ERROR; }
+                }
+                //SAME X ASPECT
+                else if (this.X == root.X)
+                {
+                    //TOP ASPECT
+                    if (this.Z > root.Z)
+                        return Direction.TOP;
+                    //BOTTOM ASPECT
+                    else if (this.Z < root.Z)
+                        return Direction.BOTTOM;
+                    //SAME Z ASPECT
+                    else if (this.Z == root.Z)
+                        return Direction.NONE;
+                    else { return Direction.ERROR; }
+                }
+                else { return Direction.ERROR; }
+            }
+            else
+                return Direction.ERROR;
         }
     }
 }
